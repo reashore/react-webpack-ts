dac96ea255efdbff74eb7d3965f328d644a1376f
Frank J. Reashore <reashore@telus.net>
2019-02-23 11:22:09 -0800

    Added mode to webpack

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 const App: React.SFC = () => {
-  return <h1>My React App!</h1>;
+  return <h1>My React and TypeScript3 App</h1>;
 };
 
-ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
+const root: HTMLElement | null = document.getElementById("root");
+
+ReactDOM.render(<App />, root);
